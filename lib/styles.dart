7f7795bc3ae1a25b39a9ba5a0ad24cb7446111ff
Morné le Roux <mornelerx@gmail.com>
2023-02-15 20:28:04 +0200

    Changed Theme

--- lib/styles.dart
+++ lib/styles.dart
@@ -4,8 +4,10 @@ import 'package:google_fonts/google_fonts.dart';
 // This is the place to edit all of the const styles etc.
 // Font sizes should be declared in the widget itself with .copywith(), as the multiplier for screen width must be in the context of the page.
 
-final TextStyle kMainHeadingTextStyle = GoogleFonts.russoOne();
+final TextStyle kMainHeadingTextStyle =
+    GoogleFonts.russoOne(color: Colors.white);
 
-final TextStyle kSecondaryHeadingTextStyle = GoogleFonts.righteous();
+final TextStyle kSecondaryHeadingTextStyle =
+    GoogleFonts.righteous(color: Colors.white);
 
-const Color kBackgroundColor = Color.fromARGB(255, 228, 255, 237);
+const Color kBackgroundColor = Color.fromARGB(255, 6, 13, 22);
