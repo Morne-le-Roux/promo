d3052acf5a9ea324257ddc05396459c103bff1bd
Morné le Roux <mornelerx@gmail.com>
2023-02-22 20:48:51 +0200

    updated build

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,8 +1,9 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:promo/absentry_appcard.dart';
+import 'package:promo/hi_im.dart';
 import 'package:promo/styles.dart';
-import 'hi_im.dart';
+import 'package:simplified_flutter_animations/generic_fade_transition.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -29,24 +30,41 @@ class _MainScreenState extends State<MainScreen> {
         //Note to self, don't put a scaffold in a SingleChildScrollView
 
         child: Column(children: [
-          const HiImMorne(), //Text that displays... Hi, I'm Morne le Roux, a flutter dev.
+          GenericFadeTransition(
+              duration: const Duration(
+                milliseconds:
+                    2500, // Animation length - slightly shorter as the rest to break up the website a bit.
+              ),
+              curve: Curves.fastOutSlowIn,
+              builder: (context) {
+                return const HiImMorne();
+              }), //Text that displays... Hi, I'm Morne le Roux, a flutter dev.
+          // Now with an animation!
 
           const SizedBox(height: 40), //spacing above the app cards
 
-          CarouselSlider(
-            items: appList,
-            //carousel. cuz every great website needs a carousel of annoying movement that keeps on distracting your eyes.
-            options: CarouselOptions(
-              height: 1000,
+          GenericFadeTransition(
+              duration: const Duration(
+                milliseconds:
+                    3500, // Animation length - slightly longer as the rest to break up the website a bit.
+              ),
+              curve: Curves.fastOutSlowIn,
+              builder: (context) {
+                return CarouselSlider(
+                  items: appList,
+                  //carousel. cuz every great website needs a carousel of annoying movement that keeps on distracting your eyes.
+                  options: CarouselOptions(
+                    height: 1000,
 
-              //TODO: this looks good for 1080p screens and portrait phone stuff. Need to work on the in between still
-              viewportFraction: screenWidth > 1500 ? 0.15 : 0.5,
-              enlargeCenterPage: true,
-              enlargeFactor: 0.15,
-              autoPlay: true,
-              autoPlayInterval: const Duration(milliseconds: 4000),
-            ),
-          ),
+                    //TODO: this looks good for 1080p screens and portrait phone stuff. Need to work on the in between still
+                    viewportFraction: screenWidth > 1500 ? 0.15 : 0.5,
+                    enlargeCenterPage: true,
+                    enlargeFactor: 0.15,
+                    autoPlay: true,
+                    autoPlayInterval: const Duration(milliseconds: 4000),
+                  ),
+                );
+              }),
         ]),
       ),
     );
